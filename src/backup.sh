4d16522bfbc6d9557779a9c4e6c0da935e31e99d
alex_pi <alexandruandrei659.aa@gmail.com>
2025-11-30 23:16:07 +0100

    refactor: update backup script and Dockerfile for improved backup handling and organization

--- src/backup.sh
+++ src/backup.sh
@@ -1,26 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-# Nome dell'immagine del container PostgreSQL
-IMAGE_NAME="postgres:latest"
-
-# Ottieni l'ID del container
-CONTAINER_ID=$(docker ps -q --filter "ancestor=$IMAGE_NAME")
-
-# Variabili
-BACKUP_DIR="backup"   # Sostituisci con il percorso in cui desideri salvare i backup
+BACKUP_DIR="/backup"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 BACKUP_FILE="$BACKUP_DIR/backup.sql"
 
 # Crea la directory di backup se non esiste
 mkdir -p $BACKUP_DIR
 
-# Cancella tutto il contenuto della directory di backup
-rm -f $BACKUP_DIR/*
+# Cancella i backup più vecchi di 7 giorni prima di crearne uno nuovo
+find $BACKUP_DIR -type f -name "backup_*.sql" -mtime +7 -exec rm {} \;
 
 # Esegui il backup escludendo la tabella "network_devices"
-docker exec $CONTAINER_ID pg_dump -U postgres --exclude-table=network_devices sensor_data > $BACKUP_FILE
+PGPASSWORD=$DB_PASSWORD pg_dump -h $DB_HOST -U $DB_USER --exclude-table=network_devices $DB_DATABASE > $BACKUP_FILE
 
-# Rimuovi i backup più vecchi di 7 giorni
-find $BACKUP_DIR -type f -name "*.sql" -mtime +7 -exec rm {} \;
-
-echo "Backup completato. File salvato in: $BACKUP_FILE"
+# Verifica se il backup è andato a buon fine
+if [ $? -eq 0 ]; then
+    echo "Backup completato con successo. File salvato in: $BACKUP_FILE"
+    exit 0
+else
+    echo "Errore durante il backup!"
+    exit 1
+fi
